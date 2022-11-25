beaea59d3698ff4f6e4c32463b053ba36d3352a4
Denis Rozhkov <al1ennnnn@yandex.ru>
2022-11-25 10:44:36 +0400

    'changed'

---
+++ task_03/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+internal class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            int denominator = 2;
+            bool flag = false;
+
+            Write("\nВведите любое целое положительное число: ");
+            int number = int.Parse(ReadLine());
+
+            while (denominator < number)
+            {
+                if (number % denominator == 0
+                    && denominator != number)
+                    flag = true;
+                denominator += 1;
+            }
+
+            if (!flag)
+                WriteLine($"Число {number} является простым числом.");
+            else
+                WriteLine($"Число {number} не является простым числом.");
+        }
+    }
+}
